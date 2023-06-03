d07b96d6c5af522f40fc8df9e47c6384b5e9ad19
yangBasak <yangbasak@gamil.com>
2023-06-03 17:37:37 +0900

    Design: 그림자 및 높이값 수정, 함수 축약형으로 변경

--- src/components/parts/card.tsx
+++ src/components/parts/card.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 
-type CardProps = {
-  children: React.ReactNode
-}
-
-const Card = ({children}: CardProps) => {
-  return <CardStyle>{children}</CardStyle>
-}
+const Card = ({children}: {children: React.ReactNode}) => (
+  <CardStyle>{children}</CardStyle>
+)
 
 export default Card
 
 const CardStyle = styled.div`
   background-color: #fff;
-  height: 420px;
+  height: 390px;
   width: 100%;
-  max-width: 463px;
-  border-radius: 5px;
-  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
+  max-width: 400px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  box-shadow: 2px 3px 12px rgba(0, 0, 0, 0.2);
 `
