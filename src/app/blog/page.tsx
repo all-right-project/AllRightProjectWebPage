60b1365f70fbe580f7f3f1f3d31348aae52428d9
jiny <jiny3360@naver.com>
2023-06-04 18:09:27 +0900

    페이지 임시 퍼블리싱

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,6 +9,32 @@ const page = (props: Props) => {
   return (
     <BlogContainer>
       <h1>기술 블로그</h1>
+      <div>
+        <div>
+          <h2>최근 게시물</h2>
+          <div>슬라이더 박스</div>
+        </div>
+        <div>
+          <div>
+            <ul>
+              <li>모두보기</li>
+              <li>인기</li>
+              <li>프론트엔드</li>
+              <li>백엔드</li>
+              <li>기타</li>
+            </ul>
+            <div>
+              검색
+              <input type="text" placeholder="카테고리 / 게시물 검색" />
+            </div>
+          </div>
+          <div>검색 결과</div>
+        </div>
+        <div>
+          <h2>게시물</h2>
+          <div>게시물 리스트</div>
+        </div>
+      </div>
     </BlogContainer>
   )
 }
