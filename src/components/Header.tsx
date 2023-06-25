f7ec0adfc90cff716b7e9cb4cf7dbeb0d8e67668
jiny <jiny3360@naver.com>
2023-06-25 14:45:55 +0900

    ✨Feat: 모바일 메뉴바 퍼블리싱하기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,14 @@
 import Link from 'next/link'
 import React from 'react'
 import styled from 'styled-components'
+import MenuBar from '@/assets/menu_bar.png'
+import Image from 'next/image'
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = React.useState(false)
+  const handleMenu = () => {
+    setOpenMenu(!openMenu)
+  }
   return (
     <HeaderContainer>
       <LogoBox>
@@ -28,7 +34,36 @@ export default function Header() {
             <Link href="/issue">Issue</Link>
           </li>
         </ul>
+        <div className="mobile">
+          <Image
+            src={MenuBar}
+            width={20}
+            height={20}
+            alt="menuBar"
+            onClick={handleMenu}
+          />
+        </div>
       </NaviBox>
+
+      {openMenu && (
+        <PopUpMenu>
+          <li>
+            <Link href="/member">Member</Link>
+          </li>
+          <li>
+            <Link href="/project">Project</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/study">Study</Link>
+          </li>
+          <li>
+            <Link href="/issue">Issue</Link>
+          </li>
+        </PopUpMenu>
+      )}
     </HeaderContainer>
   )
 }
@@ -53,6 +88,9 @@ const LogoBox = styled.div`
 const NaviBox = styled.div`
   ul {
     display: flex;
+    @media (max-width: 1280px) {
+      display: none;
+    }
     li {
       margin-left: 20px;
       a:hover {
@@ -61,8 +99,29 @@ const NaviBox = styled.div`
       }
     }
   }
-
-  @media (max-width: 768px) {
+  .mobile {
+    display: none;
+    @media (max-width: 1280px) {
+      display: block;
+    }
+  }
+`
+const PopUpMenu = styled.ul`
+  display: flex;
+  position: absolute;
+  left: 0;
+  right: 0;
+  width: 80%;
+  margin: 0 auto;
+  justify-content: space-evenly;
+  background-color: #fff;
+  margin-top: 100px;
+  border: 1px solid #ccc;
+  padding: 20px 0;
+  border-radius: 10px;
+  li {
+  }
+  @media (min-width: 1280px) {
     display: none;
   }
 `
